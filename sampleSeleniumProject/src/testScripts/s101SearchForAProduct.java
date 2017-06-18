177a1df67964b89e3bd6c018dd5102fa287051d2
kranthi k <287kiran@gmail.com>
2017-06-18 19:37:15 -0400

    updated

--- sampleSeleniumProject/src/testScripts/s101SearchForAProduct.java
+++ sampleSeleniumProject/src/testScripts/s101SearchForAProduct.java
@@ -1,6 +1,7 @@
 package testScripts;
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,7 +19,7 @@ public class s101SearchForAProduct extends ParserDriverObject{
 	public void s101SearchForAProductTest(){
 		obj.homePageObj().mEnterSearchInput("Hello World");
 	}
-	@AfterMethod
+	@AfterTest
 	public void close(){
 		obj.parserDriverObj().mClose();
 	}
